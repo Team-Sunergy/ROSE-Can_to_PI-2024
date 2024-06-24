ca9987a96da03ac101b27934bf382061efaa00f5
blaezj <blaezjib@icloud.com>
2024-06-24 16:22:37 -0400

    font size changes

--- gui.py
+++ gui.py
@@ -24,6 +24,9 @@ leftWindow.rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), weight=1)
 leftWindow.grid_propagate(False) # makes it so grid doesnt expand based on labels inside
 
 rightWindow = tk.Frame(mainWin, borderwidth=5, relief='raised')
+rightWindow.columnconfigure(0, weight=1)
+rightWindow.columnconfigure(1, weight=1)
+rightWindow.columnconfigure(2, weight=1)
 rightWindow.rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9), weight=1)
 rightWindow.grid_propagate(False)
 
@@ -38,18 +41,18 @@ speedActual = ttk.Label(leftWindow, text="inf spd", font=("Helvetica", "65", "bo
 speedActual.grid(row=5, column=1, sticky='n')
 
 # right window labels
-socLabel = ttk.Label(rightWindow, text=" SOC ", font=("Helvetica", "30"), borderwidth=5, relief='raised')
-motorCurrentInLabel = ttk.Label(rightWindow, text=" MOTOR CURRENT IN: ", font=("Helvetica", "30"), borderwidth=2, relief='raised')
-motorCurrentOutLabel = ttk.Label(rightWindow, text=" ZACH METER: ", font=("Helvetica", "30"),borderwidth=2, relief='raised')
-deltaVoltageLabel = ttk.Label(rightWindow, text=" DELTA VOLTAGE: ", font=("Helvetica", "30"), borderwidth=2, relief='raised')
-HappinessStatusLabel = ttk.Label(rightWindow, text=" HAPPINESS STATUS: HAPPY!!!", font=("Helvetica", "30"), borderwidth=2, relief='raised')
+socLabel = ttk.Label(rightWindow, text=" SOC ", font=("Helvetica", "15"), borderwidth=5, relief='raised')
+motorCurrentInLabel = ttk.Label(rightWindow, text=" MOTOR CURRENT IN: ", font=("Helvetica", "15"), borderwidth=2, relief='raised')
+motorCurrentOutLabel = ttk.Label(rightWindow, text=" ZACH METER: ", font=("Helvetica", "15"),borderwidth=2, relief='raised')
+deltaVoltageLabel = ttk.Label(rightWindow, text=" DELTA VOLTAGE: ", font=("Helvetica", "15"), borderwidth=2, relief='raised')
+HappinessStatusLabel = ttk.Label(rightWindow, text=" HAPPINESS STATUS: HAPPY!!!", font=("Helvetica", "15"), borderwidth=2, relief='raised')
 
 
-socLabel.grid(row=2)
-motorCurrentInLabel.grid(row=3)
-motorCurrentOutLabel.grid(row=4)
-deltaVoltageLabel.grid(row=5)
-HappinessStatusLabel.grid(row=6)
+socLabel.grid(row=2, column=0)
+motorCurrentInLabel.grid(row=3,column=0)
+motorCurrentOutLabel.grid(row=4, column=0)
+deltaVoltageLabel.grid(row=5, column=0)
+HappinessStatusLabel.grid(row=6, column=0)
 
 def startGui():
     """starts the gui loop given data"""
