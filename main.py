26ae160199bd9727e84c3d7a18e60f18022c3855
blaezj <blaezjib@icloud.com>
2024-06-26 15:10:20 -0400

    Removed GUI Interface for Main Branch

--- main.py
+++ main.py
@@ -9,98 +9,14 @@ import queue
 import threading
 from mockDataTransfer import *
 
-
-
-# window
-mainWin = tk.Tk()
-mainWin.title("raspberry pi interface")
-mainWin.geometry('800x480')
-
-# configures columns
-mainWin.columnconfigure(0, weight=1)
-mainWin.columnconfigure(1, weight=1)
-mainWin.rowconfigure(0, weight=1)
-
-# configure left window
-leftWindow = tk.Frame(mainWin, borderwidth=5, relief='raised')
-leftWindow.columnconfigure(0, weight=1)
-leftWindow.columnconfigure(1, weight=10)
-leftWindow.columnconfigure(2, weight=1)
-leftWindow.rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), weight=1)
-leftWindow.grid_propagate(False) # makes it so grid doesnt expand based on labels inside
-
-rightWindow = tk.Frame(mainWin, borderwidth=5, relief='raised')
-rightWindow.columnconfigure(0, weight=1)
-rightWindow.columnconfigure(1, weight=1)
-rightWindow.columnconfigure(2, weight=1)
-rightWindow.rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9), weight=1)
-rightWindow.grid_propagate(False)
-
-# place widgets
-rightWindow.grid(row=0, column=1, sticky='nsew')
-leftWindow.grid(row=0, column=0, sticky='nsew')
-
-# place widget in left window
-speedCarLabel = ttk.Label(leftWindow, text="SPEED", font=('Helvetica', '10'))
-speedCarLabel.grid(row=4, column=1, sticky='s')
-speedActual = ttk.Label(leftWindow, text="inf spd", font=("Helvetica", "65", "bold"), borderwidth=2, relief="raised")
-speedActual.grid(row=5, column=1, sticky='n')
-
-# right window labels
-socLabel = ttk.Label(rightWindow, text=" SOC ", font=("Helvetica", "15"), borderwidth=2, relief='raised')
-motorCurrentInLabel = ttk.Label(rightWindow, text=" MOTOR CURRENT IN: ", font=("Helvetica", "15"), borderwidth=2, relief='raised')
-motorCurrentOutLabel = ttk.Label(rightWindow, text=" ZACH METER: ", font=("Helvetica", "15"),borderwidth=2, relief='raised')
-deltaVoltageLabel = ttk.Label(rightWindow, text=" DELTA VOLTAGE: ", font=("Helvetica", "15"), borderwidth=2, relief='raised')
-HappinessStatusLabel = ttk.Label(rightWindow, text=" HAPPINESS STATUS: HAPPY!!!", font=("Helvetica", "15"), borderwidth=2, relief='raised')
-
-socLabel.grid(row=2, column=0, sticky='w')
-motorCurrentInLabel.grid(row=3, column=0, sticky='w')
-motorCurrentOutLabel.grid(row=4, column=0, sticky='w')
-deltaVoltageLabel.grid(row=5, column=0, sticky='w')
-HappinessStatusLabel.grid(row=6, column=0, sticky='w')
-
-def startGui():
-    """starts the gui loop given data"""
-    print("Starting gui")
-    mainWin.mainloop()
-
-def updateGuiData(dataQueue):
-    """updates gui via a queue system"""
-    try:
-        # non-blocking get from queue
-        data = dataQueue.get_nowait()
-    except queue.Empty:
-        # no new data
-        pass
-    else:
-        # data received, update labels
-        update_label(data=data)
-
-    # schedule next update
-    mainWin.after(100, updateGuiData, dataQueue)
-
         
-def update_label(data: dict):
-        """private for gui.py, takes data dict
-        and updates label"""
-        if data['DataType'] != 'none':
-            # update speed with speed
-            speedActual.config(text=str(data['Speed']))
-            socLabel.config(text=" SOC: " + str(data['SOC']))
-            motorCurrentInLabel.config(text=" MOTOR CURRENT IN: " + str(data['MotorCurrentPeakAverage']))
-            motorCurrentOutLabel.config(text= " ZACH METER: " + str(data['FETTemperature']))
-            deltaVoltageLabel.config(text = " DELTA VOLTAGE: ") # + str(float(data['HighCellVolts']) - float( data['LowCellVolts'])))
-        else:
-             speedActual.config(text="none")
-             socLabel.config(text="datatype = none")
-
 def worker_thread(queue, bus):
     """A worker thread that generates canData and puts it on the queue."""
     print("Running worker thread.")
     while True:
         data = canCollection(bus)
         queue.put(data) # puts data in queue
-        time.sleep(1)  # controls the rate of data generation.
+        time.sleep(0.1)  # controls the rate of data generation.
 
 def canCollection(bus):
     print("In the try")
@@ -154,8 +70,8 @@ def main():
     print("Sending request frame0 in main...")
     worker = threading.Thread(target=worker_thread, args=(dataQueue, bus))
     worker.start()
-    mainWin.after(100, updateGuiData, dataQueue)
-    startGui()
+    #mainWin.after(100, updateGuiData, dataQueue)
+    #startGui()
 
 if __name__ == "__main__":
     main()
